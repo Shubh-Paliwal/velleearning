19509ce4404669601bced16d0ad2aeb9b041cca2
Shubh-Paliwal <shubhpaliwal2005@gmail.com>
2026-07-25 23:23:02 +0530

    Configure environment variables for production

--- velleearning/settings.py
+++ velleearning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,16 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zk=90h#d6xy(1o!+zxid-@%0y()!uc!)h&u&$w@*to9gc)^r!&'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-zk=90h#d6xy(1o!+zxid-@%0y()!uc!)h&u&$w@*to9gc)^r!&"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    ".onrender.com",
-]
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost,.onrender.com"
+).split(",")
 
 
 # Application definition
